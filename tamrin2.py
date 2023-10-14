7cc6e4bd07c7c96f2dd7f29d81d07756108c9ef8
nargesmohmdii <147082366+nargesmohmdii@users.noreply.github.com>
2023-10-14 01:36:28 -0700

    Add files via upload

---
+++ tamrin2.py
@@ -0,0 +1,22 @@
+username = {
+  "name": "narges",
+  "family": "mohamadi",
+  "year": 2000
+}
+ 
+print(username)
+
+#dictionary  قابل تغییر است #
+
+
+friends = ["fateme", "zahra", "moyan", "fazi"]
+friends.remove("moyan")
+print(friends)
+
+#list  قابل تغییر است #
+
+
+uni = ("hosh", "mohandesi", "dadekavi")
+print(uni)
+
+#tuple غیر قابل تغییر است #
